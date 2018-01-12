4369ad6f1f209856f9a3878a9488f6bce7d97ad1
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-01-12 10:51:52 -0500

    Fix silly mistakes

--- bin/sync.js
+++ bin/sync.js
@@ -1,7 +1,9 @@
-const MANIFEST_JSON = '../manifest.json'
-const PACKAGE_JSON = '../package.json'
-
 const fs = require('fs')
+const path = require('path')
+
+const MANIFEST_JSON = path.resolve(__dirname, '../manifest.json')
+const PACKAGE_JSON = path.resolve(__dirname, '../package.json')
+
 const manifest = require(MANIFEST_JSON)
 const pkg = require(PACKAGE_JSON)
 
@@ -10,10 +12,12 @@ const copyKeys = [
   'description'
 ]
 
-for (key in copyKeys) {
-  manifest[key] = pkg[key]
-}
+const updates = copyKeys.reduce((accum, key) => {
+  accum[key] = pkg[key]
+  return accum
+}, {})
+const new_manifest = Object.assign({}, manifest, updates)
 
-fs.writeFileSync(MANIFEST_JSON, JSON.stringify(manifest, null, 4), 'utf8')
+fs.writeFileSync(MANIFEST_JSON, JSON.stringify(new_manifest, null, 4), 'utf8')
 
 console.log('Sync manifest with package.json complete!')
